a32886680b50215d44e35347e2d5dec4c7892c0a
92eduardocastillo <92eduardocastillo@gmail.com>
2026-02-02 07:41:09 +0100

    Remove prueba.html and update favicon path in Layout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,18 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/portafoliio-astro/',
-  outDir: './docs',
-  site: 'https://92eduardocastillo.github.io',
-  build: {
-    format: 'file',
-    assets: '_astro'
-  },
   vite: {
     plugins: [tailwindcss()]
-  },
-  devToolbar: {
-    enabled: true,
-    placement: 'bottom-left',
   },
 });
